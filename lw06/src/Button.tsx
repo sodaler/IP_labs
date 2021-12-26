9963ed87aa8cefea085183514140afbeb4bb2684
sodaler <misha_lector87@mail.ru>
2021-12-26 15:47:26 +0300

    Lab6_Done
    Lab6_Done

---
+++ lw06/src/Button.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface ButtonProps {
+    text: string;
+    onClick: () => any; 
+}
+
+var Button = (props: ButtonProps) => {
+    return (
+        <button onClick={props.onClick}>{props.text}</button>
+    );
+};
+export default Button;
